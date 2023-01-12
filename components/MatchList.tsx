1111b611e4d6e0dfe5df5af3eca696a91ecbac90
Sean McGeachie <sean@seans-air.home>
2023-01-12 15:37:00 +0000

    adding matching terms functionality

--- components/MatchList.tsx
+++ components/MatchList.tsx
@@ -27,13 +27,52 @@ const removeBtnStyle = (isLight: boolean) => ({
 
 export default function MatchList(){
   const store = useStore(); 
-  const [localTermList, setLocalTermList] = useState<string[]>();
-  const [localCaptionList, setLocalCaptionList] = useState<string[]>();
+  const [localTermList, setLocalTermList] = useState<string[]>([]);
+  const [matches, setMatches] = useState([]);
+  const [matches2, setMatches2] = useState({});
+  const [matchDict, setMatchDict] = useState({
+    "shank":[5.02, 4.10]
+    })
 
   //runs every time store.urlList is changed 
   useEffect(() => {
     setLocalTermList(store.termsList);
   }, [store.termsList]);
+
+  useEffect(() => {
+    //logging captions for dev
+    store.urlList.forEach(url => console.log(url.captions))
+  }, [store.urlList]);
+
+  //run once on component load
+  useEffect(() => {
+    //iterate through caption data
+    //look for words or phrases within data
+    //should we look for exacts or like 80% match?
+    store.urlList.forEach(url => matchCaptions(localTermList, url.captions))
+  }, [localTermList])
+  
+
+  //takes string arr, loops through captions arr and finds matches
+  function matchCaptions(terms:string[], captions:object[]){
+      for(let j = 0; j < captions.length; j++){
+        for(let i = 0; i < terms.length; i++){
+          if(captions[j].text.includes(terms[i])){
+            const timestamp = captions[j].start;
+            console.log(matchDict[terms[i]])
+            if(matchDict[terms[i]] && !matchDict[terms[i]].includes(timestamp)){
+              const arr = matchDict[terms[i]]
+              arr.push(timestamp)
+            }
+            else {
+              matchDict[terms[i]] = [timestamp]
+            }
+
+          }
+        }
+      }
+  }
+
 
   return (
     <section id="url-list">
@@ -41,11 +80,13 @@ export default function MatchList(){
         return(
           <Card className="url-list-" key={i}>
             <h3 className="url-link">{term}</h3>
+            <h4>{matchDict[term]}</h4>
             <div id="flex-container" style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
             </div>
           </Card>
         );
       })}
+
     </section>
   );
 }
