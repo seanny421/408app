53381191f5a0738a38c92d73d5a58b4b780d1c39
Sean McGeachie <sean@seans-air.home>
2023-03-06 18:07:50 +0000

    fixing hyrdration issue on timline.tsx

--- components/Editor/Timeline.tsx
+++ components/Editor/Timeline.tsx
@@ -1,12 +1,19 @@
 import useStore from "../../global/state"
 import {CutVideoObject} from "../../global/types"
+import { useState, useEffect } from "react"
 
 export default function Timeline(){
   const store = useStore()
+  const [timelineVideos, setTimelineVideos] = useState<CutVideoObject[]>([])
+
+  useEffect(() => {
+    setTimelineVideos(store.timelineVideos)
+  }, [])
+
   return(
     <div>
       <h3>Timeline</h3>
-      {store.timelineVideos.map(function(video:CutVideoObject, i:number){
+      {timelineVideos.map(function(video:CutVideoObject, i:number){
         return (
           <h4 key={i}>{video.id}</h4>
         )
